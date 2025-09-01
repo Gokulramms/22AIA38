4e9268fba3fa9ab4ca391702abdca81c7d287d5d
Gokulramms <gokulramms@gmail.com>
2025-09-01 11:45:17 +0530

    Creating LoggingMeddleware as sparate folder

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const {save,get,exists}=require("./urlstore.js");
 const {generateShortcode,isExpired,isoExpiry}=require("./util.js");
-const logging=require("./LoggingMeddleware.js");
+const logging=require("../LoggingMeddleware");
 const app=express();
 app.use(express.json());
 app.use(logging);
